44b0479580b2df70d2414782a48e6b0bb27be741
ukulililixl <ukulililixl@gmail.com>
2017-02-15 16:16:55 +0800

    simple hardcode

--- railway/railway.py
+++ railway/railway.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
-
+from selenium import webdriver
 from splinter.browser import Browser
 from time import sleep
 import traceback
 import sys
+import lxml.html
 
 # username and passwd
 username=sys.argv[1]
@@ -15,49 +15,50 @@ initmy_url = "https://kyfw.12306.cn/otn/index/initMy12306"
 
 #html ids
 loginid="login_user"
-unameid="loginUserDTO.user_name"
-passwdid="userDTO.password"
+unameid="username"
+passwdid="password"
 
 #cookies
-starts=u"%u6DF1%u5733%u5317%2CIOQ"
-ends=u"%u5B9C%u660C%u4E1C%2CHAN"
-dtime=u"2017-02-16"
+starts=u'%u6DF1%u5733%u5317%2CIOQ'
+ends=u'%u5B9C%u660C%u4E1C%2CHAN'
+dtime=u'2017-02-16'
 order=2
 
-browser=Browser('chrome')
-browser.visit(ticket_url)
+driver=webdriver.Chrome()
+driver.get(ticket_url)
 
-print browser.is_element_present_by_id(loginid)
-
-while browser.is_element_present_by_id(loginid):
-	sleep(1)
-
-	# login
-	browser.find_by_id(u"login_user").click()
-	browser.fill(unameid, username)
-	browser.fill(passwdid, passwd)
+while driver.find_element_by_id(loginid):
+ 	# login
+	driver.find_element_by_id(loginid).click()
+	filluname=driver.find_element_by_id(unameid)
+	filluname.send_keys(username)
+	fillpasswd=driver.find_element_by_id(passwdid)
+	fillpasswd.send_keys(passwd)
 
 	while True:
-		if browser.url != initmy_url:
+		if driver.current_url != initmy_url:
 			sleep
 		else:
 			break
 	
 	# login over
-	if browser.url == initmy_url:
+	if driver.current_url == initmy_url:
 		break
 
-browser.visit(ticket_url)
+driver.get(ticket_url)
 
-browser.cookies.add({"_jc_save_fromStation": starts})
-browser.cookies.add({"_jc_save_toStation": ends})
-browser.cookies.add({"_jc_save_fromDate": dtime})
-browser.reload()
+driver.add_cookie({'name':'_jc_save_fromStation','value':starts})
+driver.add_cookie({'name':'_jc_save_toStation','value':ends})
+driver.add_cookie({'name':'_jc_save_fromDate','value':dtime})
+driver.refresh()
 
-while browser.url == ticket_url:
-	browser.find_by_id(u"query_ticket").click()
- 	ticket=browser.find_link_by_href('javascript:')
-	print ticket
+while driver.current_url == ticket_url:
+	driver.find_element_by_id(u"query_ticket").click()
+	tickets=driver.find_elements_by_class_name(u"btn72")
+	while len(tickets)==0:
+		sleep(1)
+		tickets=driver.find_elements_by_class_name(u"btn72")
+	tickets[1].click()
 	break
-
-
+# 
+# 
